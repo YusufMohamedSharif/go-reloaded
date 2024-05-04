c79426f04637d500a5d841311b26a5a6d56d78a7
Yusuf Sharif <yusufmohamedsharif@hotmail.com>
2024-05-04 23:32:51 +0300

    All panics fixed

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+/*
+Check new line issue, it should allow new line charachter, but it's giving me error when adding multiple lines in the sample file
+*/
 package main
 
 import (
@@ -18,7 +21,6 @@ func containsSubstring(str, substr string) bool {
 	}
 	return false
 }
-
 
 func isFileEmpty(filename string) bool {
 	// Open the file
@@ -59,7 +61,7 @@ func main() {
 	if isFileEmpty(os.Args[1]) {
 		fmt.Println("The file is empty")
 		return
-	} 
+	}
 	// else {
 	// 	fmt.Println("The file is not empty")
 	// }
@@ -71,8 +73,8 @@ func main() {
 	givenText, _ := os.ReadFile(args[0])
 
 	for _, letter := range givenText {
-		if (letter > 126 || letter < 32) && letter != '\n' {
-			fmt.Println("Wrong input!")
+		if (letter > 126 || letter < 32) && (letter != '\n' && letter != 13 && letter != 10) {
+			fmt.Println("Wrong input check input charachters!")
 			return
 		}
 	}
@@ -99,48 +101,51 @@ func main() {
 		words := strings.Split(line, " ")
 		// Print each word
 		//for i, words[i]:= range words {
-			for i :=0; i<len(words); i++ {
+		for i := 0; i < len(words); i++ {
 			// fmt.Println(words)
 			// if words[i]== "\n" {
 			// 	counter++
 			// 	fmt.Println(" count = ", counter)
 			// }
-			if words[i] == "(up)" || words[i]== "(UP)" || words[i]== "(Up)" || containsSubstring(words[i], "(up)") || containsSubstring(words[i], "(Up)") || containsSubstring(words[i], "(UP)") {
+			if words[i] == "(up)" || words[i] == "(UP)" || words[i] == "(Up)" || containsSubstring(words[i], "(up)") || containsSubstring(words[i], "(Up)") || containsSubstring(words[i], "(UP)") {
 				if i == 0 {
+					fmt.Println("Wrong input")
+					return
 					words = append(words[:i], words[i+1:]...)
 				} else {
-					if words[i]!= "(up)" && containsSubstring(words[i], "(up)") {
+					if words[i] != "(up)" && containsSubstring(words[i], "(up)") {
 						result := strings.Replace(words[i], "(up)", "", -1)
 						words[i-1] += result
-					} else if words[i]!= "(Up)" && containsSubstring(words[i], "(Up)") {
+					} else if words[i] != "(Up)" && containsSubstring(words[i], "(Up)") {
 						result := strings.Replace(words[i], "(Up)", "", -1)
 						words[i-1] += result
-					} else if words[i]!= "(UP)" && containsSubstring(words[i], "(UP)") {
+					} else if words[i] != "(UP)" && containsSubstring(words[i], "(UP)") {
 						result := strings.Replace(words[i], "(UP)", "", -1)
 						words[i-1] += result
 					}
 
 					words[i-1] = strings.ToUpper(words[i-1])
-					if i < len(words){
+					if i < len(words) {
 						words = append(words[:i], words[i+1:]...)
 					} else {
 						words = append(words[:i], words[i+1:]...)
 					}
-					
-					
+
 				}
 				i--
-			} else if words[i]== "(low)" || words[i]== "(LOW)" || words[i]== "(Low)" || containsSubstring(words[i], "(low)") || containsSubstring(words[i], "(Low)") || containsSubstring(words[i], "(LOW)") {
+			} else if words[i] == "(low)" || words[i] == "(LOW)" || words[i] == "(Low)" || containsSubstring(words[i], "(low)") || containsSubstring(words[i], "(Low)") || containsSubstring(words[i], "(LOW)") {
 				if i == 0 {
+					fmt.Println("Wrong input")
+					return
 					words = append(words[:i], words[i+1:]...)
 				} else {
-					if words[i]!= "(low)" && containsSubstring(words[i], "(low)") {
+					if words[i] != "(low)" && containsSubstring(words[i], "(low)") {
 						result := strings.Replace(words[i], "(low)", "", -1)
 						words[i-1] += result
-					} else if words[i]!= "(Low)" && containsSubstring(words[i], "(Low)") {
+					} else if words[i] != "(Low)" && containsSubstring(words[i], "(Low)") {
 						result := strings.Replace(words[i], "(Low)", "", -1)
 						words[i-1] += result
-					} else if words[i]!= "(LOW)" && containsSubstring(words[i], "(LOW)") {
+					} else if words[i] != "(LOW)" && containsSubstring(words[i], "(LOW)") {
 						result := strings.Replace(words[i], "(LOW)", "", -1)
 						words[i-1] += result
 					}
@@ -149,17 +154,19 @@ func main() {
 					words = append(words[:i], words[i+1:]...)
 				}
 				i--
-			} else if words[i]== "(cap)" || words[i]== "(CAP)" || words[i]== "(Cap)" || containsSubstring(words[i], "(cap)") || containsSubstring(words[i], "(Cap)") || containsSubstring(words[i], "(CAP)") {
+			} else if words[i] == "(cap)" || words[i] == "(CAP)" || words[i] == "(Cap)" || containsSubstring(words[i], "(cap)") || containsSubstring(words[i], "(Cap)") || containsSubstring(words[i], "(CAP)") {
 				if i == 0 {
+					fmt.Println("Wrong input")
+					return
 					words = append(words[:i], words[i+1:]...)
 				} else {
-					if words[i]!= "(cap)" && containsSubstring(words[i], "(cap)") {
+					if words[i] != "(cap)" && containsSubstring(words[i], "(cap)") {
 						result := strings.Replace(words[i], "(cap)", "", -1)
 						words[i-1] += result
-					} else if words[i]!= "(Cap)" && containsSubstring(words[i], "(Cap)") {
+					} else if words[i] != "(Cap)" && containsSubstring(words[i], "(Cap)") {
 						result := strings.Replace(words[i], "(Cap)", "", -1)
 						words[i-1] += result
-					} else if words[i]!= "(CAP)" && containsSubstring(words[i], "(CAP)") {
+					} else if words[i] != "(CAP)" && containsSubstring(words[i], "(CAP)") {
 						result := strings.Replace(words[i], "(CAP)", "", -1)
 						words[i-1] += result
 					}
@@ -178,81 +185,120 @@ func main() {
 					}
 					words = append(words[:i], words[i+1:]...)
 				}
-				
+
 				i--
-			} else if words[i]== "(hex)" || words[i]== "(HEX)" || words[i]== "(Hex)" || containsSubstring(words[i], "(hex)") || containsSubstring(words[i], "(Hex)") || containsSubstring(words[i], "(HEX)") {
+			} else if words[i] == "(hex)" || words[i] == "(HEX)" || words[i] == "(Hex)" || containsSubstring(words[i], "(hex)") || containsSubstring(words[i], "(Hex)") || containsSubstring(words[i], "(HEX)") {
 				if i == 0 {
+					fmt.Println("Wrong input")
+					return
 					words = append(words[:i], words[i+1:]...)
 				} else {
 					words[i-1] = HextoInt(words[i-1])
 					words = append(words[:i], words[i+1:]...)
 				}
 				i--
-			} else if words[i]== "(bin)" || words[i]== "(BIN)" || words[i]== "(Bin)" || containsSubstring(words[i], "(bin)") || containsSubstring(words[i], "(Bin)") || containsSubstring(words[i], "(BIN)") {
+			} else if words[i] == "(bin)" || words[i] == "(BIN)" || words[i] == "(Bin)" || containsSubstring(words[i], "(bin)") || containsSubstring(words[i], "(Bin)") || containsSubstring(words[i], "(BIN)") {
 				if i == 0 {
+					fmt.Println("Wrong input")
+					return
 					words = append(words[:i], words[i+1:]...)
 				} else {
 					words[i-1] = BintoInt(words[i-1])
 					words = append(words[:i], words[i+1:]...)
 				}
 				i--
-			} else if words[i]== "(up," || words[i]== "(UP," || words[i]== "(Up," || containsSubstring(words[i], "(up,") || containsSubstring(words[i], "(Up,") || containsSubstring(words[i], "(UP,") {
+			} else if words[i] == "(up," || words[i] == "(UP," || words[i] == "(Up," || containsSubstring(words[i], "(up,") || containsSubstring(words[i], "(Up,") || containsSubstring(words[i], "(UP,") {
 				if i == 0 {
 					fmt.Println("Wrong input")
+					return
 					// words = append(words[:i], words[i+2:]...)
 				} else if i < len(words)-1 {
 					closingParanIndex := 0
-					b := strings.Trim(string(words[i+1]), words[i+1][len(words[i+1])-1:])
+					for j := 0; j < len(words[i+1]); j++ {
+						if words[i+1][j] == ')' {
+							closingParanIndex = j
+							break
+						}
+					}
+					//b := strings.Trim(string(words[i+1]), words[i+1][len(words[i+1])-1:])
+					b := strings.Trim(string(words[i+1]), words[i+1][closingParanIndex:])
 					number, _ := strconv.Atoi(string(b))
 					for j := 1; j <= number; j++ {
 						if i-j >= 0 {
 							words[i-j] = strings.ToUpper(words[i-j])
 						}
 					}
-					for j := 0; j < len(words[i+1]); j++{
-						if 	words[i+1][j] == ')'{
-							closingParanIndex = j+1
+					for j := 0; j < len(words[i+1]); j++ {
+						if words[i+1][j] == ')' {
+							closingParanIndex = j + 1
+							words[i+1] = words[i+1][closingParanIndex:]
+							words[i-1] += words[i+1]
+							break
+						} else if words[i+1][j] == '}' || words[i+1][j] == ']' {
+							closingParanIndex = j
 							words[i+1] = words[i+1][closingParanIndex:]
 							words[i-1] += words[i+1]
 							break
 						}
-						
+
 					}
 					words = append(words[:i], words[i+2:]...)
 				}
 				i--
-			} else if words[i]== "(low," || words[i]== "(LOW," || words[i]== "(Low," || containsSubstring(words[i], "(low,") || containsSubstring(words[i], "(Low,") || containsSubstring(words[i], "(LOW,") {
+			} else if words[i] == "(low," || words[i] == "(LOW," || words[i] == "(Low," || containsSubstring(words[i], "(low,") || containsSubstring(words[i], "(Low,") || containsSubstring(words[i], "(LOW,") {
 				if i == 0 {
 					fmt.Println("Wrong input")
+					return
 					// words = append(words[:i], words[i+2:]...)
 				} else if i < len(words)-1 {
 					closingParanIndex := 0
-					b := strings.Trim(string(words[i+1]), words[i+1][len(words[i+1])-1:])
+					for j := 0; j < len(words[i+1]); j++ {
+						if words[i+1][j] == ')' {
+							closingParanIndex = j
+							break
+						}
+					}
+					//b := strings.Trim(string(words[i+1]), words[i+1][len(words[i+1])-1:])
+					b := strings.Trim(string(words[i+1]), words[i+1][closingParanIndex:])
+
 					number, _ := strconv.Atoi(string(b))
 					for j := 1; j <= number; j++ {
 						if i-j >= 0 {
 							words[i-j] = strings.ToLower(words[i-j])
 						}
 					}
-					for j := 0; j < len(words[i+1]); j++{
-						if 	words[i+1][j] == ')'{
-							closingParanIndex = j+1
+					for j := 0; j < len(words[i+1]); j++ {
+						if words[i+1][j] == ')' {
+							closingParanIndex = j + 1
+							words[i+1] = words[i+1][closingParanIndex:]
+							words[i-1] += words[i+1]
+							break
+						} else if words[i+1][j] == '}' || words[i+1][j] == ']' {
+							closingParanIndex = j
 							words[i+1] = words[i+1][closingParanIndex:]
 							words[i-1] += words[i+1]
 							break
 						}
-						
+
 					}
 					words = append(words[:i], words[i+2:]...)
 				}
 				i--
-			} else if words[i]== "(cap," || words[i]== "(CAP," || words[i]== "(Cap," || containsSubstring(words[i], "(cap,") || containsSubstring(words[i], "(Cap,") || containsSubstring(words[i], "(CAP,") {
+			} else if words[i] == "(cap," || words[i] == "(CAP," || words[i] == "(Cap," || containsSubstring(words[i], "(cap,") || containsSubstring(words[i], "(Cap,") || containsSubstring(words[i], "(CAP,") {
 				if i == 0 {
 					fmt.Println("Wrong input")
+					return
 					// words = append(words[:i], words[i+2:]...)
 				} else if i < len(words)-1 {
 					closingParanIndex := 0
-					b := strings.Trim(string(words[i+1]), words[i+1][len(words[i+1])-1:])
+					for j := 0; j < len(words[i+1]); j++ {
+						if words[i+1][j] == ')' {
+							closingParanIndex = j
+							break
+						}
+					}
+					//b := strings.Trim(string(words[i+1]), words[i+1][len(words[i+1])-1:])
+					b := strings.Trim(string(words[i+1]), words[i+1][closingParanIndex:])
 					number, _ := strconv.Atoi(string(b))
 					for j := 1; j <= number; j++ {
 						if i-j >= 0 {
@@ -270,15 +316,20 @@ func main() {
 							}
 						}
 					}
-					for j := 0; j < len(words[i+1]); j++{
-						if 	words[i+1][j] == ')'{
-							closingParanIndex = j+1
+					for j := 0; j < len(words[i+1]); j++ {
+						if words[i+1][j] == ')' {
+							closingParanIndex = j + 1
+							words[i+1] = words[i+1][closingParanIndex:]
+							words[i-1] += words[i+1]
+							break
+						} else if words[i+1][j] == '}' || words[i+1][j] == ']' {
+							closingParanIndex = j
 							words[i+1] = words[i+1][closingParanIndex:]
 							words[i-1] += words[i+1]
 							break
 						}
-						
-					}  
+
+					}
 					words = append(words[:i], words[i+2:]...)
 				}
 				i--
@@ -298,18 +349,18 @@ func main() {
 				defer secondfile.Close()
 			}
 			// Write the content to the file
-		_, err = secondfile.Write([]byte(needed))
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			os.Exit(1)
-		}
+			_, err = secondfile.Write([]byte(needed))
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				os.Exit(1)
+			}
 
-		_, err = secondfile.Write([]byte{'\n'})
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			os.Exit(1)
-		}
-		
+			_, err = secondfile.Write([]byte{'\n'})
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				os.Exit(1)
+			}
+
 		} else {
 			secondfile, err := os.OpenFile(args[1], os.O_WRONLY|os.O_TRUNC, 0o644)
 			if err != nil {
@@ -318,21 +369,19 @@ func main() {
 				defer secondfile.Close()
 			}
 			// Write the content to the file
-		_, err = secondfile.Write([]byte(needed))
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			os.Exit(1)
-		}
+			_, err = secondfile.Write([]byte(needed))
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				os.Exit(1)
+			}
 
-		_, err = secondfile.Write([]byte{'\n'})
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			os.Exit(1)
+			_, err = secondfile.Write([]byte{'\n'})
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				os.Exit(1)
+			}
+			counter++
 		}
-		counter ++
-		}
-
-		
 
 	}
 
@@ -493,7 +542,7 @@ func BintoInt(bin string) string {
 func ChangeA(s []string) []string {
 	vowels := []string{"a", "e", "i", "o", "u", "h", "A", "E", "I", "O", "U", "H"}
 
-	for i, word:= range s {
+	for i, word := range s {
 		for _, letter := range vowels {
 			if i+1 < len(s) && len(s[i+1]) > 0 && word == "a" && string(s[i+1][0]) == letter {
 				s[i] = "an"
